f1693bc5d1a7bb456d84887147717091d380e996
Suntipap <88478908+Suntipap1312@users.noreply.github.com>
2022-04-12 12:09:01 +0700

    first commit

---
+++ index.go
@@ -0,0 +1,13 @@
+func main() {
+	// Create a new token object, specifying signing method and the claims
+	// you would like it to contain.
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": "admin",
+		"password": "1234,
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString(hmacSampleSecret)
+
+	fmt.Println(tokenString, err)
+}
